e89569d58a5ba55a8a20bb5935f4e4c5775d0588
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:38:29 +0200

    Create CUtilNatives.h

---
+++ Shared/Scripting/Natives/CUtilNatives.h
@@ -0,0 +1,30 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CUtilNatives.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+class CScriptingManager;
+class CUtilNatives
+{
+
+private:
+
+	static	SQInteger		PlayerIdFromName( SQVM * pVM );
+	static	SQInteger		WeaponNameFromId( SQVM * pVM );
+	static	SQInteger		WeaponIdFromName( SQVM * pVM );
+	static	SQInteger		ToRGBA( SQVM * pVM );
+	static	SQInteger		FromRGB( SQVM * pVM );
+
+	static	SQInteger		Clamp( SQVM * pVM );
+
+public:
+
+	static	void			Register( CScriptingManager * pScriptingManager );
+
+};
