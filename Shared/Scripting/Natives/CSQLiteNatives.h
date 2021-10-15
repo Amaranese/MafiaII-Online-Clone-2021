3dccf51072fa6a048bd03819f7d9abfe74f2f676
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:38:44 +0200

    Create CSQLiteNatives.h

---
+++ Shared/Scripting/Natives/CSQLiteNatives.h
@@ -0,0 +1,18 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CSQLiteNatives.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+class CScriptingManager;
+
+class CSQLiteNatives
+{
+public:
+	static	void			Register( CScriptingManager * pScriptingManager );
+};
