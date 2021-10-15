86b2662c51a7db042b9257e42b8b90be9f61cb58
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:31:36 +0200

    Create CAudioNatives.h

---
+++ Client/CAudioNatives.h
@@ -0,0 +1,21 @@
+/*************************************************************
+*
+* Solution   : Mafia2-Online
+* Project    : Client
+* File       : CAudioNatives.h
+* Developers : Tyldar <tyldar@mafia2-online.com>
+*
+***************************************************************/
+
+#pragma once
+
+class CAudioNatives
+{
+
+private:
+
+public:
+
+	static	void			Register(CScriptingManager * pScriptingManager);
+
+};
