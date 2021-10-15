fbeec814047be5b2233a21a718dec94ae69e461d
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:33:53 +0200

    Create CFont.cpp

---
+++ Client/CFont.cpp
@@ -0,0 +1,38 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Client
+* File       : CFont.cpp
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#include "BaseInc.h"
+
+#include "CFont.h"
+#include "CCore.h"
+
+#include "CString.h"
+
+#include "CGraphics.h"
+
+extern CCore		* pCore;
+
+CFont::CFont( String strName, unsigned int uiSize, bool bBold )
+{
+	// Set variables
+	m_pFont = NULL;
+	m_strName = strName;
+	m_uiSize = uiSize;
+	m_bBold = bBold;
+}
+
+CFont::~CFont( void )
+{
+	SAFE_DELETE( m_pFont );
+}
+
+bool CFont::Load( void )
+{
+	return SUCCEEDED( D3DXCreateFont( pCore->GetGraphics()->GetDevice(), m_uiSize, 0, (m_bBold ? FW_BOLD : FW_NORMAL), 1, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, (DEFAULT_PITCH | FF_DONTCARE), m_strName.Get(), &m_pFont ) );
+}
