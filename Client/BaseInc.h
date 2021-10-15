bc9347e399e92b0c2a8dbedc98953eaa11ff9bd0
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:31:09 +0200

    Create BaseInc.h

---
+++ Client/BaseInc.h
@@ -0,0 +1,24 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Client
+* File       : BaseInc.h
+* Brief      : M2-Online client precompiled header.
+*
+***************************************************************/
+
+#pragma once
+
+#ifdef _WIN32
+#define		NOMINMAX
+#include	<Windows.h>
+#include	<windowsx.h>
+#endif
+
+#include	<list>
+
+#include	<assert.h>
+
+#include 	"CCommon.h"
+
+// EOF
