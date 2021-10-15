4a3f7a262e618a3a21963369fa4dd3a0bffc2ab4
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:42:23 +0200

    Create CZlib.h

---
+++ Shared/CZlib.h
@@ -0,0 +1,25 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CZlib.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+#include <stdio.h>
+#include "CString.h"
+
+#include "zlib.h"
+
+class CZlib
+{
+
+public:
+
+	static	int			Inflate( FILE * pSource, FILE * pDestination );
+	static	int			Decompress( String strFileName, String strOutput );
+
+};
