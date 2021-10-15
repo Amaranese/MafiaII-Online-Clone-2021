21b0c0c599546db4b0fe8988982e657604e19560
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:45:34 +0200

    Create CServerNatives.h

---
+++ Server/CServerNatives.h
@@ -0,0 +1,39 @@
+#ifndef C_SERVER_NATIVES_H
+#define C_SERVER_NATIVES_H
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Server
+* File       : CServerNatives.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+#include "StdInc.h"
+
+class CServerNatives
+{
+
+private:
+
+	static	SQInteger		SetGameModeText( SQVM * pVM );
+	static	SQInteger		SetMapName( SQVM * pVM );
+	static	SQInteger		SetWeather( SQVM * pVM );
+	static	SQInteger		SetSummer( SQVM * pVM );
+	static	SQInteger		GetNetworkStats( SQVM * pVM );
+	static	SQInteger		GetWhoWasList( SQVM * pVM );
+	static	SQInteger		ClearWhoWas( SQVM * pVM );
+	static	SQInteger		GetWhoWasCount( SQVM * pVM );
+	static	SQInteger		Shutdown( SQVM * pVM );
+	static	SQInteger		StartResource( SQVM * pVM );
+	static	SQInteger		StopResource( SQVM * pVM );
+	static	SQInteger		RestartResource( SQVM * pVM );
+
+public:
+
+	static	void			Register( CScriptingManager * pScriptingManager );
+
+};
+#endif
