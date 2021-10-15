79c7dd674fc9d546674b2a50d9579b45cd8f5990
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:38:33 +0200

    Create CTimerNatives.h

---
+++ Shared/Scripting/Natives/CTimerNatives.h
@@ -0,0 +1,17 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CTimerNatives.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+class CScriptingManager;
+class CTimerNatives
+{
+public:
+	static	void			Register( CScriptingManager * pScriptingManager );
+};
