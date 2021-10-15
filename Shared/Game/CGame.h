9ae8cedbd528ea7f058e9bebb614c9edff26b08e
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:42:15 +0200

    Create CGame.h

---
+++ Shared/Game/CGame.h
@@ -0,0 +1,32 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CGame.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+#include "CString.h"
+
+namespace Game
+{
+
+	unsigned int		GetIdFromPlayerModel			( String strModel );
+	void				GetPlayerModelFromId			( unsigned int uiModel, String * strModel, String * strDirectroy );
+
+	unsigned int		GetIdFromVehicleModel			( String strModel );
+	void				GetVehicleModelFromId			( unsigned int uiModel, String * strModel );
+
+	unsigned int		GetIdFromVehicleWheelModel		( String strModel );
+	String				GetVehicleWheelModelFromId		( unsigned int uiModel );
+
+	unsigned int		GetWeaponIdFromName				( String strName );
+	String				GetWeaponNameFromId				( unsigned int uiModel );
+
+	unsigned int		GetIdFromDoorName				(String doorName);
+	String				GetDoorNameFromId				(unsigned int doorId);
+
+};
