891b6acc2f900c94ae0fdd7285be6fcdbe1fc016
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-15 15:38:27 +0200

    Create CXMLNatives.h

---
+++ Shared/Scripting/Natives/CXMLNatives.h
@@ -0,0 +1,19 @@
+/*************************************************************
+*
+* Solution   : Mafia 2 Multiplayer
+* Project    : Shared Library
+* File       : CXMLNatives.h
+* Developers : AaronLad <aaron@m2-multiplayer.com>
+*
+***************************************************************/
+
+#pragma once
+
+class CScriptingManager;
+class CXMLNatives
+{
+
+public:
+
+	static	void			Register( CScriptingManager * pScriptingManager );
+};
